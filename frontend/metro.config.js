83d28cae99339e6d9d35b48d8832eabd5ca81a71
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2026-02-23 19:44:20 +0530

    fix: Update metro.config.js to remove NativeWind integration and adjust API_BASE_URL in api.ts to use production URL

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./globals.css" });
+module.exports = config;
